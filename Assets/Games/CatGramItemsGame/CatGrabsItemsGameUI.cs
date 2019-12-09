243192688b257ce36b76858aacaf529aca23a176
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-12-09 01:06:15 +0100

    Adds a text for the time

--- Assets/Games/CatGramItemsGame/CatGrabsItemsGameUI.cs
+++ Assets/Games/CatGramItemsGame/CatGrabsItemsGameUI.cs
@@ -8,12 +8,12 @@ public class CatGrabsItemsGameUI : MonoBehaviour
     public GameObject canvas;
     public CatGrabsItemsGame game;
     public Text catchedTargets;
+    public Text timer;
 
     private void OnEnable()
     {
         game.OnGameLoaded.AddListener(OnGameWasLoaded);
         game.OnTargetCatched.AddListener(UpdateCatchedTargets);
-        
     }
 
     protected void OnGameWasLoaded()
@@ -22,9 +22,36 @@ public class CatGrabsItemsGameUI : MonoBehaviour
         UpdateInfo();
     }
 
+    private void Update()
+    {
+        switch (game.GetState())
+        {
+            case BaseGame.State.unloaded:
+                break;
+            case BaseGame.State.loaded:
+                break;
+            case BaseGame.State.started:
+                UpdateTimer();
+                break;
+            case BaseGame.State.quit:
+                canvas.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
     private void UpdateInfo()
     {
+        UpdateTimer();
         UpdateCatchedTargets();
+    }
+
+    protected void UpdateTimer()
+    {
+        float time = game.remainingTime;
+        time = Mathf.Max(time, 0.0f);
+        timer.text = time.ToString();
     }
 
     protected void UpdateCatchedTargets()
