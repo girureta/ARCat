208745ccb4d5d5bf06eed85f759e42fe9719e7ea
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-12-09 16:01:14 +0100

    Adds a particle system that are spawn the the fish is caught.

--- Assets/Scripts/Fish/FishTargetController.cs
+++ Assets/Scripts/Fish/FishTargetController.cs
@@ -9,13 +9,27 @@ public class FishTargetController : MonoBehaviour
 {
     public UnityEvent OnCatchedByCat = new UnityEvent();
 
+    public Transform particleSpawnPoint;
+    public ParticleSystem targedCatchedParticlesPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
         CatController cat = other.GetComponent<CatController>();
         if(cat != null)
         {
+            SpawnParticles();
             OnCatchedByCat.Invoke();
             Destroy(gameObject);
         }
+    }
+
+    protected void SpawnParticles()
+    {
+        var particles = GameObject.Instantiate(targedCatchedParticlesPrefab);
+        var scale = particles.transform.localScale;
+        particles.transform.transform.SetParent(transform.parent);
+        scale.Scale(transform.parent.lossyScale);
+        particles.transform.localScale = scale;
+        particles.transform.position = particleSpawnPoint.position;
     }
 }
