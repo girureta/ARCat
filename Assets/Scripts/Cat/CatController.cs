918b704a6fccbe4e5f92188ceb79426b01252dfe
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-12-09 16:43:26 +0100

    Defines some states for the cat. Moves cats smoothly toward the target. Adds triggers for the AnimatorController.

--- Assets/Scripts/Cat/CatController.cs
+++ Assets/Scripts/Cat/CatController.cs
@@ -4,9 +4,49 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+    protected enum State
+    {
+        idle,
+        walking
+    }
+
+    protected State state = State.idle;
+    protected Vector3 target = Vector3.zero;
+    protected Vector3 currentVelocity = Vector3.zero;
+
+    public Animator animator;
+    protected int animatorIdleTrigger = Animator.StringToHash("idle");
+    protected int animatorWalkTrigger = Animator.StringToHash("walk");
 
     public void MoveTo(Vector3 position)
     {
-        transform.position = position;
+        target = position;
+        SetWalk();
+    }
+
+    private void Update()
+    {
+        if (state == State.walking)
+        {
+            float distance = Vector3.Distance(transform.position, target);
+            float timeToReach = distance / 4.0f;
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref currentVelocity, timeToReach);
+            if (distance < 0.05)
+            {
+                SetIdle();
+            }
+        }
+    }
+
+    protected void SetWalk()
+    {
+        state = State.walking;
+        animator.SetTrigger(animatorWalkTrigger);
+    }
+
+    protected void SetIdle()
+    {
+        state = State.idle;
+        animator.SetTrigger(animatorIdleTrigger);
     }
 }
